44b65f8b89b76ae58b3539d906e4002a85eadbd0
John U. Balis <phobossystems@gmail.com>
2021-08-17 02:54:14 -0500

    added patch for b_batch update when loading a PPO2 model and corresponding test

--- tests/test_ppo2.py
+++ tests/test_ppo2.py
@@ -1,8 +1,11 @@
 import os
 
 import pytest
+import gym 
 
 from stable_baselines import PPO2
+from stable_baselines.common import make_vec_env
+from stable_baselines.common.vec_env import DummyVecEnv
 
 
 @pytest.mark.parametrize("cliprange", [0.2, lambda x: 0.1 * x])
@@ -19,3 +22,18 @@ def test_clipping(cliprange, cliprange_vf):
 
     if os.path.exists('./ppo2_clip.zip'):
         os.remove('./ppo2_clip.zip')
+
+def test_update_n_batch_on_load():
+    env = make_vec_env('CartPole-v1',n_envs=2)
+    model = PPO2('MlpPolicy', env, n_steps = 10, nminibatches=1)
+    
+    model.learn(total_timesteps = 100)
+    model.save("ppo2_cartpole")
+    
+    del model
+    
+    model = PPO2.load("ppo2_cartpole")
+    test_env = DummyVecEnv([lambda: gym.make('CartPole-v1')])
+    
+    model.set_env(test_env)
+    model.learn(total_timesteps = 100)
